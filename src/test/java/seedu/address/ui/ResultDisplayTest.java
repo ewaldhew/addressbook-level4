567b46f20b6cce314657a60a572dd00c7e098e94
Ewald Hew <ewaldhew@gmail.com>
2018-03-08 22:25:41 +0800

    Update all tests for `ResultDisplay' changes
    - ResultDisplayHandle: Add a new function to get style classes for
    checking.
    - ResultDisplayTest: Add default and error style checks.
    - AddressBookSystemTest: Add `ResultDisplay' tests to existing test
    procedures for `CommandBox'.
    - *CommandSystemTest: Update references.

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -3,6 +3,8 @@ package seedu.address.ui;
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,6 +18,9 @@ public class ResultDisplayTest extends GuiUnitTest {
 
     private ResultDisplayHandle resultDisplayHandle;
 
+    private ArrayList<String> defaultStyleOfResultDisplay;
+    private ArrayList<String> errorStyleOfResultDisplay;
+
     @Before
     public void setUp() {
         ResultDisplay resultDisplay = new ResultDisplay();
@@ -23,6 +28,11 @@ public class ResultDisplayTest extends GuiUnitTest {
 
         resultDisplayHandle = new ResultDisplayHandle(getChildNode(resultDisplay.getRoot(),
                 ResultDisplayHandle.RESULT_DISPLAY_ID));
+
+        defaultStyleOfResultDisplay = new ArrayList<>(resultDisplayHandle.getStyleClass());
+
+        errorStyleOfResultDisplay = new ArrayList<>(defaultStyleOfResultDisplay);
+        errorStyleOfResultDisplay.add(ResultDisplay.ERROR_STYLE_CLASS);
     }
 
     @Test
@@ -30,8 +40,24 @@ public class ResultDisplayTest extends GuiUnitTest {
         // default result text
         guiRobot.pauseForHuman();
         assertEquals("", resultDisplayHandle.getText());
+        assertEquals(defaultStyleOfResultDisplay, resultDisplayHandle.getStyleClass());
 
         // new result received
+        assertMessageAndStyleCorrect(NEW_RESULT_EVENT_STUB, defaultStyleOfResultDisplay);
+        assertMessageAndStyleCorrect(NEW_RESULT_ERROR_STUB, errorStyleOfResultDisplay);
+    }
+
+    /**
+     * Posts given event and verifies that the <br>
+     *     - the text is the expected message <br>
+     *     - the style is the same as {@code expectedStyle}.
+     * @param event
+     * @param expectedStyle
+     */
+    private void assertMessageAndStyleCorrect(NewResultAvailableEvent event, ArrayList<String> expectedStyle) {
+        postNow(event);
         guiRobot.pauseForHuman();
+        assertEquals(event.message, resultDisplayHandle.getText());
+        assertEquals(expectedStyle, resultDisplayHandle.getStyleClass());
     }
 }
