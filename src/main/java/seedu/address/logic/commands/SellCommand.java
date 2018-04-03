2cbc6194acb44bb130a5bb0985148e48eb1a7170
Ewald Hew <ewaldhew@gmail.com>
2018-04-03 18:53:55 +0800

    Add `sell` command
    * Register in main parser
    * Move prefix checker into multimap itself

---
+++ src/main/java/seedu/address/logic/commands/SellCommand.java
@@ -0,0 +1,106 @@
+//@@author ewaldhew
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_COINS;
+
+import java.util.List;
+import java.util.Objects;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.coin.Coin;
+import seedu.address.model.coin.exceptions.CoinNotFoundException;
+import seedu.address.model.coin.exceptions.DuplicateCoinException;
+
+/**
+ * Removes value from an existing coin in the book.
+ */
+public class SellCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "sell";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Removes value from the coin account identified "
+            + "by the index number used in the last coin listing. "
+            + "Parameters: INDEX (must be a positive integer) "
+            + "AMOUNT\n"
+            + "Example: " + COMMAND_WORD + " 1 " + "50.0";
+
+    public static final String MESSAGE_SELL_COIN_SUCCESS = "Sold: %1$s";
+    public static final String MESSAGE_NOT_SOLD = "No amount entered.";
+
+    private final Index index;
+    private final double amountToSell;
+
+    private Coin coinToEdit;
+    private Coin editedCoin;
+
+    /**
+     * @param index of the coin in the filtered coin list to change
+     * @param amountToSell of the coin
+     */
+    public SellCommand(Index index, double amountToSell) {
+        requireNonNull(index);
+
+        this.index = index;
+        this.amountToSell = amountToSell;
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        try {
+            model.updateCoin(coinToEdit, editedCoin);
+        } catch (DuplicateCoinException dpe) {
+            throw new CommandException("Unexpected code path!");
+        } catch (CoinNotFoundException pnfe) {
+            throw new AssertionError("The target coin cannot be missing");
+        }
+        model.updateFilteredCoinList(PREDICATE_SHOW_ALL_COINS);
+        return new CommandResult(String.format(MESSAGE_SELL_COIN_SUCCESS, coinToEdit));
+    }
+
+    @Override
+    protected void preprocessUndoableCommand() throws CommandException {
+        List<Coin> lastShownList = model.getFilteredCoinList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_COIN_DISPLAYED_INDEX);
+        }
+
+        coinToEdit = lastShownList.get(index.getZeroBased());
+        editedCoin = createEditedCoin(coinToEdit, amountToSell);
+    }
+
+    /**
+     * Creates and returns a {@code Coin} with the details of {@code coinToEdit}
+     * edited with {@code editCoinDescriptor}.
+     */
+    private static Coin createEditedCoin(Coin coinToEdit, double amountToSell) {
+        assert coinToEdit != null;
+
+        Coin editedCoin = new Coin(coinToEdit);
+        editedCoin.addTotalAmountSold(amountToSell);
+
+        return editedCoin;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof SellCommand)) {
+            return false;
+        }
+
+        // state check
+        SellCommand e = (SellCommand) other;
+        return index.equals(e.index)
+                && amountToSell == e.amountToSell
+                && Objects.equals(coinToEdit, e.coinToEdit);
+    }
+}
--- src/main/java/seedu/address/logic/parser/SellCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SellCommandParser.java
@@ -5,40 +5,36 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.TokenType.PREFIXAMOUNT;
 
-import java.util.EmptyStackException;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.BuyCommand;
+import seedu.address.logic.commands.SellCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new BuyCommand object
+ * Parses input arguments and creates a new SellCommand object
  */
-public class BuyCommandParser implements Parser<BuyCommand> {
+public class SellCommandParser implements Parser<SellCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the BuyCommand
      * and returns an BuyCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public BuyCommand parse(String args) throws ParseException {
+    public SellCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenizeToArgumentMultimap(args, PREFIXAMOUNT);
         if (!argMultimap.arePrefixesPresent(PREFIXAMOUNT)
                 || argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BuyCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SellCommand.MESSAGE_USAGE));
         }
 
         try {
             Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            double amountToAdd = ParserUtil.parseDouble(argMultimap.getValue(PREFIXAMOUNT).get());
-            return new BuyCommand(index, amountToAdd);
+            double amountToSell = ParserUtil.parseDouble(argMultimap.getValue(PREFIXAMOUNT).get());
+            return new SellCommand(index, amountToSell);
         } catch (IllegalValueException ive) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BuyCommand.MESSAGE_USAGE));
-        } catch (EmptyStackException ese) {
-            throw new ParseException(BuyCommand.MESSAGE_NOT_BOUGHT);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SellCommand.MESSAGE_USAGE));
         }
 
     }
