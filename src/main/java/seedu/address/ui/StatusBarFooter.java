40eeab32634f7bdecfe25c7331a939f54800d013
Ewald Hew <ewaldhew@gmail.com>
2018-04-15 20:20:55 +0800

    StatusBarFooter: Add filter status

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.CoinBookChangedEvent;
+import seedu.address.commons.events.ui.FilterChangedEvent;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
@@ -22,6 +23,7 @@ public class StatusBarFooter extends UiPart<Region> {
     public static final String SYNC_STATUS_INITIAL = "Not updated yet in this session";
     public static final String SYNC_STATUS_UPDATED = "Last Updated: %s";
     public static final String ITEM_COUNT_STATUS = "%d item(s) total";
+    public static final String FIND_COMMAND_STATUS = "Current filter: %s";
 
     /**
      * Used to generate time stamps.
@@ -79,6 +81,10 @@ public class StatusBarFooter extends UiPart<Region> {
         Platform.runLater(() -> this.syncStatus.setText(status));
     }
 
+    private void setFindCommandStatus(String status) {
+        Platform.runLater(() -> this.itemCountStatus.setText(String.format(FIND_COMMAND_STATUS, status)));
+    }
+
     @Subscribe
     public void handleCoinBookChangedEvent(CoinBookChangedEvent abce) {
         long now = clock.millis();
@@ -86,5 +92,11 @@ public class StatusBarFooter extends UiPart<Region> {
         logger.info(LogsCenter.getEventHandlingLogMessage(abce, "Setting last updated status to " + lastUpdated));
         setSyncStatus(String.format(SYNC_STATUS_UPDATED, lastUpdated));
         setItemCountStatus(abce.data.getCoinList().size());
+    }
+
+    @Subscribe
+    public void handleFilterChangedEvent(FilterChangedEvent fce) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(fce, "Setting filter info..."));
+        setFindCommandStatus(fce.status);
     }
 }
